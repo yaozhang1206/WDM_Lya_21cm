60ba75506a5d3d040f6df23f61046ad1091f6f68
yaozhang1206 <98475678+yaozhang1206@users.noreply.github.com>
2022-07-02 01:22:46 +0800

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -15,9 +15,9 @@ class spec_pixel(object):
     
     def _read_file(self,mag):
         # this get the files with SNR(z,lambda)
-        if self.band is 'g':
+        if self.band == 'g':
             file = './data/sn-spec-lya-g'+str(mag)+'-t4000.dat'
-        elif self.band is 'r':
+        elif self.band == 'r':
 #            print 'Made it to r-band\n'
             file = './data/sn-spec-lya-r'+str(mag)+'-t4000.dat'
         else:
