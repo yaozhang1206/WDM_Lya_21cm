aa4faa0716dcd61c643124ff3c9e2be2ea1b5be1
Yao Zhang <98475678+yaozhang1206@users.noreply.github.com>
2024-10-16 23:01:39 +0800

    Update lya_convertor.py

--- public/code/lya_convertor.py
+++ public/code/lya_convertor.py
@@ -1,19 +1,17 @@
 import numpy as np
 from scipy.integrate import quad
 
+"""
+    This class defines the unit convertor using our fiducial cosmology.
+"""
 
 class lya_convert(object):
-    # this class defines the convertor for our fiducial cosmology, remember that this should not depend on cosmology since one usually ignore the change of the covariance with cosmology (see the verde & bernal paper (2020) on forecasts errors
-    
+
     def __init__(self):
         # need to define cosmology for hubble parameter
         self.Omega_m = 0.3088
         self.h = 0.6774
-        self.Omega_r = 8.6e-5 # for consistency with 21cmFAST, but pretty much useless here
-#        print 'Paulo it might be a good idea to use omegas instead of Omegas\n'
-        
-#        print 'Paulo check the referee report conversion factor because I might have forgotten the extra redshift factor\n' # was checked no problem. 
-    
+        self.Omega_r = 8.6e-5 # for consistency with 21cmFAST, but pretty much useless here   
 
     def hubble(self,z):
         # returns the hubble parameter as a function of z for given cosmology in the constructor
@@ -43,17 +41,12 @@ class lya_convert(object):
         return 1.0 * c_kms / (1.0 * self.hubble(z))
     
     def comoving_dist(self,z):
-        # self explanatory haha
         # NOTE THAT THIS IS SUPPOSED TO BE angular_diameter_distance(z) * (1+z), and the angular diameter distance is D_A = D_m / (1+z), with D_m the comoving distance, hence it should be just D_m
         temp = quad(self.integrand, 0, z)
-#        return temp[0] / (1.0 + z) # typo mentioned in the note above
         return temp[0]
     
     def dMpc_ddeg(self,z):
         # this one takes care of the transition from deg to Mpc, so twice for areas!!
-        # how to relate Mpc with radian? that's how D_A, the comoving angular diameter distance is defined!
-        dMpc_drad = self.comoving_dist(z) # / (1.0 + z) # activate this if I think Andreu was wrong
-        # it seems that what we needed is not the angular diameter distance but the comoving distance?
-        
-#        print('Comoving angular diameter distance = ',dMpc_drad)
+        # What we needed is not the angular diameter distance but the comoving distance because we are not calculating proper distance but the comoving distance.
+        dMpc_drad = self.comoving_dist(z)
         return dMpc_drad * np.pi / 180.0
